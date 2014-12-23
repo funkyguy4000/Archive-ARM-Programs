45c4083169e1b0ba4b971d39ecf3f58dc03855a3
Shannon Strutz <strutz.shannon@gmail.com>
2014-12-22 19:18:01 -0600

    Adding a few processor expert projects

---
+++ Processor Expert/Accelerometer MMA8451/main.c
@@ -0,0 +1,101 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Project     : Onboard Accelerometer MMA8451Q
+**     Processor   : MKL25Z128VLK4
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-11-19, 10:20, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "MMA1.h"
+#include "GI2C1.h"
+#include "WAIT1.h"
+#include "CI2C1.h"
+#include "RED.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "GREEN.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "BLUE.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+  MMA1_Init();
+  RED_Init();
+  GREEN_Init();
+  BLUE_Init();
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  RED_Off();
+  GREEN_Off();
+  BLUE_Off();
+  MMA1_Enable();
+  /* For example: for(;;) { } */
+  for(;;){
+	  RED_Put(MMA1_GetX() > 50);
+	  BLUE_Put(MMA1_GetY() > 50);
+	  GREEN_Put(MMA1_GetZ() > 50);
+  }
+  
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
